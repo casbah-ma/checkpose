d9c3f072f17d57655806e8d03cfe8994a6174281
Youssef <youssef@ctd.ma>
2023-03-13 20:51:12 -0400

    add findAngle

---
+++ src/lib/findAngle.js
@@ -0,0 +1,7 @@
+export default function findAngle(A,B,C) {
+    var AB = Math.sqrt(Math.pow(B.x-A.x,2)+ Math.pow(B.y-A.y,2));    
+    var BC = Math.sqrt(Math.pow(B.x-C.x,2)+ Math.pow(B.y-C.y,2)); 
+    var AC = Math.sqrt(Math.pow(C.x-A.x,2)+ Math.pow(C.y-A.y,2));
+    
+    return Math.acos((BC*BC+AB*AB-AC*AC) / (2*BC*AB)) * (180 / Math.PI);   
+}
