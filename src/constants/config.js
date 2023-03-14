8e724c2e6dec69d63a9fdd2b81180921a149d6e9
Youssef <youssef@ctd.ma>
2023-03-13 20:50:19 -0400

    add TENSION

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,9 +1,11 @@
 const LINE_COLOR = "orange";
 const LINE_WIDTH = 4;
 const MIN_SCORE = .3;
+const TENSION = 0
 
 export {
     LINE_COLOR,
     LINE_WIDTH,
-    MIN_SCORE
+    MIN_SCORE,
+    TENSION
 }
