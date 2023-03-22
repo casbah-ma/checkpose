483f43c504d3b976e4b78353a853393c96145817
Youssef <youssef@ctd.ma>
2023-03-22 14:52:59 -0400

    bug fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,11 +5,12 @@ position:relative;
 max-width: 600px; 
 height: 100vh;
 margin: 0px auto;
-background-color: #07090a ;
+background-color: #07090a;
+background-color: rgba(7,9,10,.98) ;
 overflow-y: ${props => props.scroll ? "scroll" : "hidden"}  ;
 border: 2px solid rgba(255,255,255,.01);
-overflow-x: hidden ;
-box-shadow: -1px -1px 0px 0px #48abe047, -4px -5px 20px 0px #48abe02b, 1px 1px 0px 0px #48abe047, 4px 5px 20px 0px #48abe02b;
+overflow-x: hidden;
+
 `
 
 export default Layout
