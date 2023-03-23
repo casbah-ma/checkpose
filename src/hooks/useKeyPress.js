93b9b5986780180b51ee22be8743ade9f15dfaf6
Youssef <youssef@ctd.ma>
2023-03-22 20:47:49 -0400

    react-hooks/exhaustive-deps

--- src/hooks/useKeyPress.js
+++ src/hooks/useKeyPress.js
@@ -32,6 +32,7 @@ export default function useKeyPress(targetKey) {
       window.removeEventListener("keydown", downHandler);
       window.removeEventListener("keyup", upHandler);
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []); // Empty array ensures that effect is only run on mount and unmount
 
   return keyPressed;
