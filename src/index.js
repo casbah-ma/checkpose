c2def05fbb6cd7c157db9185f5fb638e2b032a61
Youssef <youssef@ctd.ma>
2023-03-24 17:17:00 -0400

    update service worker register to update him self

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
-import { Toaster } from 'react-hot-toast';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import Capture from 'pages/Capture'
-import Analyze from 'pages/Analyze';
-import Intro from 'pages/Intro'
-import 'rc-slider/assets/index.css';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { toast, Toaster } from "react-hot-toast";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import Capture from "pages/Capture";
+import Analyze from "pages/Analyze";
+import Intro from "pages/Intro";
+import "rc-slider/assets/index.css";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
@@ -32,16 +29,37 @@ const router = createBrowserRouter([
   {
     path: "*",
     element: <h1>NOT FOUND</h1>,
-  }
+  },
 ]);
 
-
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <>
     <Toaster />
-    <RouterProvider router={ router } />
+    <RouterProvider router={router} />
   </>
 );
 
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    const waitingServiceWorker = registration.waiting;
+
+    if (waitingServiceWorker) {
+      waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
+      toast.promise(
+        delay(window.location.reload(), 2500),
+        {
+          loading: "Updating...",
+          success: <b>Updated</b>,
+          error: <b>Update aborded</b>,
+        }
+      );
+    }
+  },
+});
+
+function delay(t) {
+  return new Promise(function (resolve) {
+      setTimeout(resolve, t);
+  });
+}
