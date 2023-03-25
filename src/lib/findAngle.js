75382e35f1af9684338de8929899d821d0329a80
Youssef <youssef@ctd.ma>
2023-03-25 16:21:50 -0400

    bug fix

--- src/lib/findAngle.js
+++ src/lib/findAngle.js
@@ -14,7 +14,7 @@ export default function findAngle(A, B, C) {
     var BC = Math.sqrt(Math.pow(B[0]-C[0],2)+ Math.pow(B[1]-C[1],2)); 
     var AC = Math.sqrt(Math.pow(C[0]-A[0],2)+ Math.pow(C[1]-A[1],2));
     
-    return Math.round(180-Math.acos((BC*BC+AB*AB-AC*AC) / (2*BC*AB)) * (180 / Math.PI));   
+    return Math.round(180-(Math.acos((BC*BC+AB*AB-AC*AC) / (2*BC*AB)) * (180 / Math.PI)));   
 }
 
 /**
